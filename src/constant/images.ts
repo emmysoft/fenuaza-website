85233a398ccbdc34d365746cc967a2148a48c7bc
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-12-24 01:05:09 +0000

    some shitts

--- src/constant/images.ts
+++ src/constant/images.ts
@@ -1,2 +1,9 @@
-export {}
+import home1 from '../assets/img/home1.png';
+import home2 from '../assets/img/home2.png';
+import home3 from '../assets/img/home3.png'
 
+export {
+    home1,
+    home3,
+    home2
+}
